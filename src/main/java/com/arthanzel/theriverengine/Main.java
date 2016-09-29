1661ed962222bcfdbfb469c0cf2eb7b6fcf87b68
Martin Hanzel <arthanzel@gmail.com>
2016-09-29 10:33:43 -0400

    JavaFX

--- src/main/java/com/arthanzel/theriverengine/Main.java
+++ src/main/java/com/arthanzel/theriverengine/Main.java
@@ -1,17 +1,7 @@
 package com.arthanzel.theriverengine;
 
-import com.arthanzel.theriverengine.rivergen.RiverArc;
-import com.arthanzel.theriverengine.rivergen.RiverNetwork;
-import com.arthanzel.theriverengine.rivergen.RiverNode;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import org.jgrapht.DirectedGraph;
-import org.jgrapht.Graph;
-import org.jgrapht.alg.DijkstraShortestPath;
-
-import java.io.IOException;
-import java.util.Random;
-import java.util.Set;
 
 /**
  * Entry point for The River Engine.
@@ -21,7 +11,8 @@ import java.util.Set;
 public class Main extends Application {
     @Override
     public void start(Stage main) {
-
+        main.setTitle("The River Engine");
+        main.show();
     }
 
     public static void main(String[] args) {
