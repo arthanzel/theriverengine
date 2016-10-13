572e304631a8276cfd50043c8a53f45d09163853
Martin Hanzel <arthanzel@gmail.com>
2016-10-12 21:35:30 -0400

    Removed idea folder because it is causing more problems than it is solving.

--- src/main/java/com/arthanzel/theriverengine/ui/RiverRenderer.java
+++ src/main/java/com/arthanzel/theriverengine/ui/RiverRenderer.java
@@ -8,6 +8,7 @@ import javafx.scene.Group;
 import javafx.scene.layout.Region;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeLineCap;
 
 /**
  * TODO: Documentation
@@ -15,14 +16,14 @@ import javafx.scene.shape.Rectangle;
  * @author Martin
  */
 public class RiverRenderer extends Region {
-    Group root;
-    Group scaleGroup;
+    private Group root;
 
     public RiverRenderer() {
         Rectangle clip = new Rectangle(this.getWidth(), this.getHeight());
         clip.widthProperty().bind(this.widthProperty());
         clip.heightProperty().bind(this.heightProperty());
         this.setClip(clip);
+        initUIEvents();
     }
 
     /**
@@ -31,6 +32,7 @@ public class RiverRenderer extends Region {
      */
     public void initialize(RiverSystem system) {
         System.out.println("Initialized renderer");
+
         root = new Group();
         this.getChildren().clear();
         this.getChildren().add(root);
@@ -41,6 +43,39 @@ public class RiverRenderer extends Region {
     public void update(RiverSystem system) {
 
     }
+
+    // region UI
+    // =========
+
+    private double originalX, originalY, rootX, rootY;
+    private void initUIEvents() {
+        // Pan on drag
+        this.setOnMousePressed(me -> {
+            originalX = me.getX();
+            originalY = me.getY();
+            rootX = root.getTranslateX();
+            rootY = root.getTranslateY();
+        });
+        this.setOnMouseDragged(me -> {
+            double dx = me.getX() - originalX;
+            double dy = me.getY() - originalY;
+            root.setTranslateX(rootX + dx);
+            root.setTranslateY(rootY + dy);
+        });
+
+        // Zoom on scroll
+        this.setOnScroll(se -> {
+            if (se.getDeltaY() > 0) {
+                root.setScaleX(root.getScaleX() * UIConstants.ZOOM_FACTOR);
+                root.setScaleY(root.getScaleY() * UIConstants.ZOOM_FACTOR);
+            } else {
+                root.setScaleX(root.getScaleX() / UIConstants.ZOOM_FACTOR);
+                root.setScaleY(root.getScaleY() / UIConstants.ZOOM_FACTOR);
+            }
+        });
+    }
+
+    // endregion
 
     // region Drawing Methods
     // ======================
@@ -56,6 +91,18 @@ public class RiverRenderer extends Region {
             this.root.getChildren().add(l);
         }
     }
+
+//    private void drawScaleBar() {
+//        scaleGroup.getChildren().clear();
+//        Line line = new Line(0, 0, 100, 0);
+//        line.setStrokeWidth(4);
+//        line.setStrokeLineCap(StrokeLineCap.SQUARE);
+//        scaleGroup.getChildren().add(line);
+//
+//        scaleGroup.setTranslateX(20);
+//        System.out.println(this.getHeight());
+//        scaleGroup.setTranslateY(this.getHeight() - 140);
+//    }
 
     // endregion
 }
--- src/main/java/com/arthanzel/theriverengine/ui/RiverView.java
+++ src/main/java/com/arthanzel/theriverengine/ui/RiverView.java
@@ -42,7 +42,6 @@ public class RiverView extends Stage {
         this.setWidth(UIConstants.INITIAL_WIDTH);
         this.setHeight(UIConstants.INITIAL_HEIGHT);
 
-        initDragging(scene, root);
         initAgentMarkers(root);
         drawNetwork();
         update();
@@ -84,34 +83,5 @@ public class RiverView extends Stage {
             root.getChildren().add(c);
             agentMarkers.put(a, c);
         }
-    }
-
-    private double originalX = 0;
-    private double originalY = 0;
-    private double rootX = 0;
-    private double rootY = 0;
-
-    private void initDragging(Scene scene, Group group) {
-        scene.setOnMousePressed(me -> {
-            originalX = me.getX();
-            originalY = me.getY();
-            rootX = group.getTranslateX();
-            rootY = group.getTranslateY();
-        });
-        scene.setOnMouseDragged(me -> {
-            double dx = me.getX() - originalX;
-            double dy = me.getY() - originalY;
-            group.setTranslateX(rootX + dx);
-            group.setTranslateY(rootY + dy);
-        });
-        scene.setOnScroll(se -> {
-            if (se.getDeltaY() > 0) {
-                group.setScaleX(group.getScaleX() * UIConstants.ZOOM_FACTOR);
-                group.setScaleY(group.getScaleY() * UIConstants.ZOOM_FACTOR);
-            } else {
-                group.setScaleX(group.getScaleX() / UIConstants.ZOOM_FACTOR);
-                group.setScaleY(group.getScaleY() / UIConstants.ZOOM_FACTOR);
-            }
-        });
     }
 }
