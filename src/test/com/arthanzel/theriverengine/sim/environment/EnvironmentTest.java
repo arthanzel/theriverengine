982c3668f93b2bd714cc255f5d6a799867aa3b1c
Martin Hanzel <arthanzel@gmail.com>
2016-11-03 11:01:14 -0400

    Revert environment tests

--- src/test/com/arthanzel/theriverengine/sim/environment/EnvironmentTest.java
+++ src/test/com/arthanzel/theriverengine/sim/environment/EnvironmentTest.java
@@ -10,4 +10,12 @@ import static org.junit.Assert.*;
  * Created by martin on 2016-10-31.
  */
 public class EnvironmentTest {
+    @Test
+    public void fromCoordinateFunction() throws Exception {
+        RiverArc arc = new RiverArc(new RiverNode("A", 0, 0), new RiverNode("B", 1, 1));
+        Environment env = Environment.fromCoordinateFunction((x, y) -> x + 2 * y);
+        assertEquals(0, env.get(arc, 0), 0.00001);
+        assertEquals(3, env.get(arc, 1 * arc.length()), 0.00001);
+        assertEquals(0.5 + 2 * 0.5, env.get(arc, 0.5 * arc.length()), 0.00001);
+    }
 }
