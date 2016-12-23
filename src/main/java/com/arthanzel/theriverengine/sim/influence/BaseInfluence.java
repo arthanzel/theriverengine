de381502c36a3eab2a3d99109fd17542d3277c95
Martin Hanzel <arthanzel@gmail.com>
2016-12-23 15:22:03 -0500

    Removed drawing and concurrent code

--- src/main/java/com/arthanzel/theriverengine/sim/influence/BaseInfluence.java
+++ src/main/java/com/arthanzel/theriverengine/sim/influence/BaseInfluence.java
@@ -1,10 +1,6 @@
 package com.arthanzel.theriverengine.sim.influence;
 
-import com.arthanzel.theriverengine.concurrent.ParallelService;
 import com.arthanzel.theriverengine.ui.BooleanBinding;
-import com.arthanzel.theriverengine.util.TextUtils;
-
-import java.util.concurrent.ExecutorService;
 
 /**
  * Base implementation of an Influence. BaseInfluence does very little except provide getters and setters for the
@@ -14,7 +10,6 @@ import java.util.concurrent.ExecutorService;
  */
 public abstract class BaseInfluence implements Influence {
     @BooleanBinding private volatile boolean enabled = true;
-    private ParallelService pool;
 
     public boolean isEnabled() {
         return enabled;
@@ -22,13 +17,5 @@ public abstract class BaseInfluence implements Influence {
 
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
-    }
-
-    public ParallelService getPool() {
-        return pool;
-    }
-
-    public void setPool(ParallelService pool) {
-        this.pool = pool;
     }
 }
--- src/main/java/com/arthanzel/theriverengine/sim/influence/Influence.java
+++ src/main/java/com/arthanzel/theriverengine/sim/influence/Influence.java
@@ -1,10 +1,6 @@
 package com.arthanzel.theriverengine.sim.influence;
 
-import com.arthanzel.theriverengine.concurrent.ParallelService;
 import com.arthanzel.theriverengine.sim.RiverSystem;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 
 /**
  * An Influence defines some operation to perform on a RiverSystem. The next state of a RiverSystem can thus be
@@ -17,7 +13,4 @@ public interface Influence {
     void setEnabled(boolean enabled);
 
     void influence(RiverSystem system, double dt);
-
-    ParallelService getPool();
-    void setPool(ParallelService pool);
 }
